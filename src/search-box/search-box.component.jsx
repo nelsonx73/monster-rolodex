0bcf8dbebf24d75ad318f86f774ae775f7d9c38e
Nelson <nelsonx73@gmail.com>
2020-06-28 19:20:06 -0400

    adding files for github pages

---
+++ src/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.style.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <div className='search'>
+        <input
+            type="search"
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    </div>
+)
